d6eabd3432bccbf59d1832165272409f16b5d3f4
leoseis <eboscrib@yahoo.com>
2026-04-15 10:06:36 +0100

    added a filter component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import EarthquakeMap from "./components/EarthquakeMap";
 
 function App() {
-
+  const [minMag, setMinMag] = useState(0);
   const [earthquakes, setEarthquakes] = useState([]);
 
   useEffect(() => {
@@ -15,12 +15,30 @@ function App() {
 
   }, []);
   console.log("EARTHQUAKES:", earthquakes);
+  const filteredEarthquakes = earthquakes.filter(
+  (eq) => eq.magnitude >= minMag
+);
   return (
     <div>
+      <div style={{ textAlign: "center", marginBottom: "10px" }}>
+  <label>Minimum Magnitude: </label>
+
+<input
+  type="range"
+  min="0"
+  max="8"
+  step="0.1"
+  value={minMag}
+  onChange={(e) => setMinMag(Number(e.target.value))}
+/>
+  <span> {minMag}</span>
+</div>
+
+
 
       <h1>Global Earthquake Monitor</h1>
 
-      <EarthquakeMap earthquakes={earthquakes} />
+      <EarthquakeMap earthquakes={filteredEarthquakes} />
 
     </div>
   );
